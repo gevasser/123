4b4d4937cf287f54bc6aeb1c9f1af2a9c9902c93
gevasser <ge.vasser@gmail.com>
2017-05-25 18:35:43 +0300

    Add files via upload

---
+++ egor/templates/bottom.php
@@ -0,0 +1,10 @@
+<div class="footer-section">
+					<div class="container">
+					
+						<div class="footer-top">
+						<p>Проект выполнили: <br> Ильянков Егор, Медведко Иван, Мащенко Алиса</p>
+						</div>						
+					</div>
+				</div>
+</body>
+</html>
